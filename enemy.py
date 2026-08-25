f9596d12e60436b9510a38b785aeea32c722faf1
SunnySide-Done <they.jie.yu_2026@nyjc.edu.sg>
2026-08-25 07:55:41 +0000

    making separate entity files

--- enemy.py
+++ enemy.py
@@ -1,23 +1,5 @@
-import random
+import entity
 
-class Enemy:
-    def __init__(self, rank):
-        # M rank is minotaur, N rank is normal monster
-        self.rank = rank
-        self.health = 100
-        
-    def attack(self) -> int:
-        """Rolls for attack damage done out of 10 and returns the attack damage"""
-        attack_damage = random.randrange(1, 11)
-        return attack_damage
-
-    def hit(self, damage: int):
-        """Changes health of monster based on the damage done by player"""
-        self.health -= damage
-        return
-
-class BigEnemy(Enemy):
-    def __init__():
+class minotaur(enetity):
+    def __init__(self):
         self.health = 200
-
-    def attack
---
+++ player.py
@@ -0,0 +1,47 @@
+import entity
+
+class player(entity):
+    def __init__(self):
+        super().__init__()
+        self.inventory = []
+    
+    def heal(self):
+        """
+        adds health to the player after drinking ambrosia
+        """
+        if self.health == 100:
+            return
+        elif self.health > 80:
+            
+        else:
+            self.health += 20
+        return
+
+    def get_inventory(self) -> list:
+        """
+        gets players inventory and displays in string
+        """
+        print(self.inventory)
+        return
+
+
+    def set_inventory(self, item: str):  
+        """
+        adds item to player inventory
+        """
+        item = item
+        print("Choose an item slot. [1-5]")
+        wanted_item_position = input("Enter: ")
+        while int(wanted_item_position) and int(wanted_item_position) < 6 and int(wanted_item_position) > 0:
+            wanted_item_position = input("Enter: ")
+        if self.inventory[wanted_item_position] is not None:
+            print("Item slot occupied. Switch item? (y/n)")
+            switch_item_choice = input("Enter: ")
+            while switch_item_choice is not "y" or "n":
+                switch_item_choice = input("Enter: ")
+            if switch_item_choice is 'y':
+                self.inventory[wanted_item_position] = item
+                return
+            else:
+                print("Choose an item slot.")
+                player.set_inventory(item)
